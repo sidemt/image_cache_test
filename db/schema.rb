3e2ba490003d74cd2b9f362e0a1c2851c44012dd
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-22 20:19:34 +0000

    Create create user action

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180821201543) do
 
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
