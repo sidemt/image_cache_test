40574e75c8cacfb6ea8d8fa73acf81fc03d3da9a
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-23 18:59:05 +0000

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
   resources :users
 
