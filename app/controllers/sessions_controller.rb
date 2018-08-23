2fcb78190285e36f999b8df37ea04c2cd5440628
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-23 19:58:05 +0000

    Create login/logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    # User login
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log in
+      log_in user
+      # Redirect to user information page after login
+      redirect_to user
+    else
+      # Show error message
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  
+  # Log in with the given user
+  def log_in(user)
+    session[:user_id] = user.id # Places a temp cookie on the user's browser
+  end
+  
+  # Return the currently logged in user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  # Return true if the user is logged in, otherwise false
+  def logged_in?
+    !current_user.nil?
+  end
 end
