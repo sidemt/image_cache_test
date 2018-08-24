513765fedff134c368e34442c98f9bc599921f72
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-24 22:52:41 +0000

    Add admin attribute to users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -32,6 +32,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
   
+  # Check that admin attribute cannot be changed via the web
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                  user: { password: "password",
+                                          password_confirmation: "password",
+                                          admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+  
   # Check that the user can edit his/her information only
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
