00eaf0092e0f0f4602531fedb7be720ebcce52dc
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-24 21:56:46 +0000

    Add authorization functions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,12 +5,17 @@ module SessionsHelper
     session[:user_id] = user.id # Places a temp cookie on the user's browser
   end
   
-  # Return the currently logged in user
+  # Returns true if the given user is the currently logged in user
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # Returns the currently logged in user
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
   
-  # Return true if the user is logged in, otherwise false
+  # Returns true if the user is logged in, otherwise false
   def logged_in?
     !current_user.nil?
   end
