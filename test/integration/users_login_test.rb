ecbdf794045ff6ee503b0c80b59b527627bd9017
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-25 20:59:57 +0000

    Update test case

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,7 +35,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     assert_redirected_to root_url
     follow_redirect!
-    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "li#user_info", count: 0
   end
