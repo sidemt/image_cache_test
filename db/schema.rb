f0e75de6a9f28af905a974743e94fd6e068d5e87
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-26 19:59:09 +0000

    Implement picture upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180825184645) do
+ActiveRecord::Schema.define(version: 20180826190136) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
