5908aa86899fc15cef14546498f971211e8e60c8
sidemt <25644062+sidemt@users.noreply.github.com>
2018-08-27 19:26:51 +0000

    Improve design

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Image Cache Test"
+    assert_select "title", "Clear Cache Test"
   end
 
 end
